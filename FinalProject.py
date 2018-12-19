3f5600b38f6909fbbbb7bd240d4b53b1474948c1
Laura <lmasciola@ucvts.org>
2018-12-19 00:28:24 +0000

    Introduces Lestrade

--- FinalProject.py
+++ FinalProject.py
@@ -1,2 +1,23 @@
-print("""Introduction:
-You will be playing as the renowned detective, Sherlock Holmes.""")
+def x():
+    x = input("")
+    return x
+
+clues = []
+places = []
+
+print("INTRUCTIONS: Welcome to the game. In this game, you play as  After every line of dialogue, press enter to continue")
+x()
+
+print("Introduction:")
+print("Your name is Sherlock Holmes, an infamous detective. You live in London in 221B Baker Street with Dr. John Watson. You are sitting in your flat when Mrs. Hudson, your landlady, asks you your opinion about the recent suicides that have been reported in the paper.")
+x()
+
+print("MRS. HUDSON: What about these suicides, then, Sherlock? Thought that would be right up your street. Three of them, exactly the same. That's a bit funny, isn't it?")
+x()
+
+
+print("You suddenly get a feeling.")
+print("YOU: Four. There's been a fourth. And there's something different this time.")
+x()
+
+print("In comes Lestrade, a detective from the local police force who often uses you as a consultant.")
